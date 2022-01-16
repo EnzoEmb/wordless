92efbb790faf4e842bcef538f5ee67840e990580
Enzo <enzoamb123@gmail.com>
2022-01-16 18:22:49 -0300

    Fix full height mobile

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Header from '../components/Header';
 import Canvas from '../components/Canvas';
 import Keyboard from '../components/Keyboard';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }, []);
+
   return (
-    <div className="mx-auto max-w-3xl flex flex-col justify-between min-h-screen py-4 sm:py-10">
+    <div className="mx-auto max-w-3xl flex flex-col justify-between py-4 sm:py-10" style={{ minHeight: 'calc(var(--vh) * 100)' }}>
       <Header />
       <Canvas />
       <Keyboard />
