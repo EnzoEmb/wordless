6f202ab71020cac5ef4f1cd53088a5edeb740968
Enzo <enzoamb123@gmail.com>
2022-01-17 20:22:00 -0300

    Added dictionary api

--- components/Word.js
+++ components/Word.js
@@ -3,16 +3,16 @@ import useGuessStore from '../store';
 import config from '../utils/config';
 
 const Word = (props) => {
+
+
   const currentGuess = useGuessStore((state) => state.currentGuess);
 
-  // logica para mostrar las letras que faltan
-  let remainingLetters = [];
-  for (let index = 0; index < (5 - (currentGuess ? currentGuess.length : 0)); index++) {
-    remainingLetters.push(<Letter key={index} />);
-  }
-
-
   if (props.currentWord) {
+    // logica para mostrar las letras que faltan
+    let remainingLetters = [];
+    for (let index = 0; index < (5 - (currentGuess ? currentGuess.length : 0)); index++) {
+      remainingLetters.push(<Letter key={index} />);
+    }
     return (
       <div className={"flex gap-2 mx-auto " + (config.DEBUG ? 'border-l-4 border-green-300' : '')}>
         {currentGuess?.map((letter, index) => (<Letter key={index} index={index} letter={letter} />))}
---
+++ pages/api/es/words/[...param].js
@@ -0,0 +1,24 @@
+// es/words/LETTERS/MAX_WORDS
+
+import fs from 'fs'
+import path from 'path'
+
+export default async function handler(req, res) {
+  const [letters, maxWords] = req.query.param
+
+  const directory = path.join(process.cwd());
+  const file = fs.readFileSync(`${directory}/dictionary/es/${letters}_letters.txt`, 'utf8');
+  const words = file.split('\n');
+
+  // fix acentos
+  const wordsFixed = words.map(word => word.normalize('NFD').replace(/[\u0300-\u036f]/g, ""));
+
+  // shuffle array
+  wordsFixed.sort(() => 0.5 - Math.random());
+
+  // limit return words
+  const wordsLimited = wordsFixed.slice(0, maxWords);
+
+
+  res.end(JSON.stringify(wordsLimited))
+}
