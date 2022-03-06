48135dbe7f17a2d724425faa748d24d6febb63e9
Enzo <enzoamb123@gmail.com>
2022-03-06 18:20:34 -0300

    add title and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,28 @@
 // pages/_document.js
 
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;600;700;800;900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+
+          <title>Wordless - Wordle en español infinito</title>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
